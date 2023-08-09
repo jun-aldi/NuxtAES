fa36602c95f089a878937197a1b58c9ea670b520
juniaraldi <aldinugraha708@gmail.com>
2023-08-10 00:22:26 +0700

    edit nav

--- layouts/nav.vue
+++ layouts/nav.vue
@@ -1,22 +1,38 @@
 <template>
 <div>
-    <nav class="p-4 bg-white border-gray-200 dark:bg-gray-900 dark:border-gray-700">
+    <nav class="p-4 bg-white border-b-2 border-gray-200">
     <div
       class="flex flex-wrap items-center justify-between max-w-screen-xl p-4 mx-auto"
     >
-      <a href="#" class="flex items-center">
+      <NuxtLink :to="{ name: 'index' }" class="flex items-center">
         <img
           src="https://flowbite.com/docs/images/logo.svg"
           class="h-8 mr-3"
           alt="Flowbite Logo"
         />
         <span
-          class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white"
+          class="self-center text-2xl font-semibold whitespace-nowrap "
           >AES NUXT</span
         >
-      </a>
+      </NuxtLink>
     </div>
   </nav>
   <slot/>
+
+<!--Footer container-->
+<footer class="text-center text-white bg-white border-grey dark:bg-neutral-600">
+  
+
+  <!--Copyright section-->
+  <div
+    class="p-4 text-center border-t-2 bg-gray-50 text-neutral-700 border-grey">
+    © 2023 Copyright:
+    <a
+      class="text-neutral-800 "
+      href="https://github.com/juniaraldi"
+      >Juniar Aldi</a
+    >
+  </div>
+</footer>
 </div>
 </template>
