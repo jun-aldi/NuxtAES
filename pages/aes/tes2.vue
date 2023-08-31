ee1cf808bf8bfcdfe29e35886ff91d3a509df38b
juniaraldi <aldinugraha708@gmail.com>
2023-09-01 00:36:27 +0700

    edit asnyc all scores

--- pages/aes/tes2.vue
+++ pages/aes/tes2.vue
@@ -1,85 +1,58 @@
 <template>
-  <div>
-    <button
-      class="px-6 py-3 mb-1 mr-1 text-sm font-bold text-white uppercase transition-all duration-150 ease-linear bg-pink-500 rounded shadow outline-none active:bg-pink-600 hover:shadow-lg focus:outline-none"
-      type="button"
-      v-on:click="toggleModal()"
-    >
-      Open regular modal
-    </button>
+  <!-- ... (your template code) ... -->
+
+  <div
+    v-if="selectedUser"
+    class="fixed inset-0 z-10 flex items-center justify-center w-screen h-screen bg-black bg-opacity-50"
+    id="element-to-convert"
+  >
+    <!-- ... (loading animation button) ... -->
+
     <div
-      v-if="showModal"
-      class="fixed inset-0 z-50 flex items-center justify-center overflow-x-hidden overflow-y-auto outline-none focus:outline-none"
+      class="w-full max-w-4xl p-8 mb-8 overflow-hidden bg-white border border-gray-200 rounded-lg dark:bg-gray-800 dark:border-gray-700 md:p-12"
     >
-      <div class="relative w-auto max-w-3xl mx-auto my-6">
-        <!--content-->
-        <div
-          class="relative flex flex-col w-full bg-white border-0 rounded-lg shadow-lg outline-none focus:outline-none"
-        >
-          <!--header-->
-          <div
-            class="flex items-start justify-between p-5 border-b border-solid rounded-t border-slate-200"
-          >
-            <h3 class="text-3xl font-semibold">Modal Title</h3>
-            <button
-              class="float-right p-1 ml-auto text-3xl font-semibold leading-none text-black bg-transparent border-0 outline-none opacity-5 focus:outline-none"
-              v-on:click="toggleModal()"
-            >
-              <span
-                class="block w-6 h-6 text-2xl text-black bg-transparent outline-none opacity-5 focus:outline-none"
+      <!-- Display user details in the modal -->
+      <h2 class="mb-4 text-xl font-semibold">Student Details</h2>
+      <div
+        class="overflow-y-auto border bg-gray-50 max-h-96 border-grey-500 print-content"
+        id="element-to-print"
+      >
+        <div ref="content" class="p-4">
+          <!-- Display detailed scores -->
+          <div v-for="(score, question, index) in selectedUser.detailedScores.scores" :key="index">
+            <div class="border-b border-grey-200">
+              <a
+                class="mt-4 mb-4 bg-green-100 text-green-800 text-xl font-medium inline-flex items-center px-2.5 py-0.5 rounded-md dark:bg-gray-700 dark:text-green-400"
               >
-                ×
-              </span>
-            </button>
+                <svg
+                  class="w-3 h-3 mr-1"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                  aria-hidden="true"
+                >
+                  <path
+                    clip-rule="evenodd"
+                    fill-rule="evenodd"
+                    d="M4 2a2 2 0 00-2 2v11a3 3 0 106 0V4a2 2 0 00-2-2H4zm1 14a1 1 0 100-2 1 1 0 000 2zm5-1.757l4.9-4.9a2 2 0 000-2.828L13.485 5.1a2 2 0 00-2.828 0L10 5.757v8.486zM16 18H9.071l6-6H16a2 2 0 012 2v2a2 2 0 01-2 2z"
+                  ></path>
+                </svg>
+                {{ question }}
+              </a>
+              <p class="jawaban">{{ score.jawaban }}</p>
+            </div>
           </div>
-          <!--body-->
-          <div class="relative flex-auto p-6">
-            <p class="my-4 text-lg leading-relaxed text-slate-500">
-              I always felt like I could do anything. That’s the main thing
-              people are controlled by! Thoughts- their perception of
-              themselves! They're slowed down by their perception of themselves.
-              If you're taught you can’t do anything, you won’t do anything. I
-              was taught I could do everything.
-            </p>
-          </div>
-          <!--footer-->
-          <div
-            class="flex items-center justify-end p-6 border-t border-solid rounded-b border-slate-200"
-          >
-            <button
-              class="px-6 py-3 mb-1 mr-1 text-sm font-bold text-red-500 uppercase transition-all duration-150 ease-linear bg-transparent border border-red-500 border-solid rounded outline-none hover:bg-red-500 hover:text-white active:bg-red-600 focus:outline-none"
-              type="button"
-              v-on:click="toggleModal()"
-            >
-              Close
-            </button>
-            <button
-              class="px-6 py-2 mb-1 mr-1 text-sm font-bold text-red-500 uppercase transition-all duration-150 ease-linear outline-none background-transparent focus:outline-none"
-              type="button"
-              v-on:click="toggleModal()"
-            >
-              Save Changes
-            </button>
+          
+          <!-- Show the total score -->
+          <div class="border-b border-grey-200">
+            <strong class="text-red-500">Total Nilai:</strong>
+            <span class="font-bold">{{ selectedUser.detailedScores.totalScore }}</span>
           </div>
         </div>
       </div>
+
+      <!-- ... (Nilai, Close, and Export PDF buttons) ... -->
+
     </div>
-    <div v-if="showModal" class="fixed inset-0 z-40 bg-black opacity-25"></div>
   </div>
 </template>
-
-<script>
-export default {
-  name: "regular-modal",
-  data() {
-    return {
-      showModal: false,
-    };
-  },
-  methods: {
-    toggleModal: function () {
-      this.showModal = !this.showModal;
-    },
-  },
-};
-</script>
